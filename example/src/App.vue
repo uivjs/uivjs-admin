2079d652f8e471133324d86602d6c5dfda8fa172
jaywcjlove <398188662@qq.com>
2021-12-22 17:46:36 +0800

    chore(website): add github corners.

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,8 +1,20 @@
 <template>
-  <router-view></router-view>
+<div>
+  <github-corners fixed target="__blank" size="65" zIndex="9999" href="https://uivjs.github.io/uivjs-admin" />
+  <router-view />
+</div>
 </template>
 <style>
 html {
   background-color: #F1F1F1;
 }
 </style>
+<script>
+import GithubCorners from '@uivjs/vue-github-corners';
+
+export default {
+  components: {
+    GithubCorners
+  }
+};
+</script>
