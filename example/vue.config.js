eea4cebce6fee1911f4f00cd99b2ad84cc76ec63
jaywcjlove <398188662@qq.com>
2022-03-04 18:51:26 +0800

    chore: fix vue config.

--- example/vue.config.js
+++ example/vue.config.js
@@ -2,7 +2,7 @@ const path = require(`path`);
 const pkg = require('./package.json');
 
 module.exports = {
-  publicPath: './',
+  publicPath: '',
   pages: {
     index: {
       entry: 'src/main.js',
